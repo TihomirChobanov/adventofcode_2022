0c16ed09f6e085d0ba1216ef9949c31e95017169
Plamen Tenev <plamen.tenev@accenture.com>
2022-12-07 13:01:05 +0200

    day 5 secondpart commit

--- src/day5SecondPart.java
+++ src/day5SecondPart.java
@@ -1,11 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.*;
 
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Stack;
-
-public class day5 {
+public class day5SecondPart {
     public static void main(String[] args) throws FileNotFoundException {
 
         Stack<Character> first = new Stack<>();
@@ -19,69 +16,84 @@ public class day5 {
         Stack<Character> nineth = new Stack<>();
         generateStacks(first, second, third, forth, fifth, sixth, seventh, eight, nineth);
         printStacks(first, second, third, forth, fifth, sixth, seventh, eight, nineth);
-
+        int counter = 1;
         File myObj = new File("C:\\Users\\Tihomir\\IdeaProjects\\adventofcode\\src\\input5.txt");
         Scanner myReader = new Scanner(myObj);
 
         while (myReader.hasNextLine()) {
+            System.out.println(counter++);
             String[] currentLine = myReader.nextLine().split(" ");
             System.out.println(Arrays.toString(currentLine));
             int numberOfElements = Integer.parseInt(currentLine[1]);
             int fromStack = Integer.parseInt(currentLine[3]);
             int toStack = Integer.parseInt(currentLine[5]);
-
+            ArrayList<Character> tenth = new ArrayList<>();
             for (int i = 0; i < numberOfElements; i++) {
                 char temp = 0;
                 if (fromStack == 1) {
                     temp = first.peek();
                     first.pop();
+                    tenth.add(temp);
                 } else if (fromStack == 2) {
                     temp = second.peek();
                     second.pop();
+                    tenth.add(temp);
                 } else if (fromStack == 3) {
                     temp = third.peek();
                     third.pop();
+                    tenth.add(temp);
                 } else if (fromStack == 4) {
                     temp = forth.peek();
                     forth.pop();
+                    tenth.add(temp);
                 } else if (fromStack == 5) {
                     temp = fifth.peek();
                     fifth.pop();
+                    tenth.add(temp);
                 } else if (fromStack == 6) {
                     temp = sixth.peek();
                     sixth.pop();
+                    tenth.add(temp);
                 } else if (fromStack == 7) {
                     temp = seventh.peek();
                     seventh.pop();
+                    tenth.add(temp);
                 } else if (fromStack == 8) {
                     temp = eight.peek();
+                    tenth.add(temp);
                     eight.pop();
                 } else if (fromStack == 9) {
                     temp = nineth.peek();
                     nineth.pop();
+                    tenth.add(temp);
                 }
-
-                if (toStack == 1) {
-                    first.push(temp);
-                } else if (toStack == 2) {
-                    second.push(temp);
-                } else if (toStack == 3) {
-                    third.push(temp);
-                } else if (toStack == 4) {
-                    forth.push(temp);
-                } else if (toStack == 5) {
-                    fifth.push(temp);
-                } else if (toStack == 6) {
-                    sixth.push(temp);
-                } else if (toStack == 7) {
-                    seventh.push(temp);
-                } else if (toStack == 8) {
-                    eight.push(temp);
-                } else if (toStack == 9) {
-                    nineth.push(temp);
-                }
-
             }
+
+            System.out.println("Tenth is : " + tenth);
+            Collections.reverse(tenth);
+            System.out.println("Reversed Tenth is : " + tenth);
+
+            if (toStack == 1) {
+                first.addAll(tenth);
+            } else if (toStack == 2) {
+                second.addAll(tenth);
+            } else if (toStack == 3) {
+                third.addAll(tenth);
+            } else if (toStack == 4) {
+                forth.addAll(tenth);
+            } else if (toStack == 5) {
+                fifth.addAll(tenth);
+            } else if (toStack == 6) {
+                sixth.addAll(tenth);
+            } else if (toStack == 7) {
+                seventh.addAll(tenth);
+            } else if (toStack == 8) {
+                eight.addAll(tenth);
+            } else if (toStack == 9) {
+                nineth.addAll(tenth);
+            }
+
+            tenth.clear();
             System.out.println("----------After changing: ");
             printStacks(first, second, third, forth, fifth, sixth, seventh, eight, nineth);
         }
@@ -91,15 +103,15 @@ public class day5 {
     }
 
     private static void printStacks(Stack<Character> first, Stack<Character> second, Stack<Character> third, Stack<Character> forth, Stack<Character> fifth, Stack<Character> sixth, Stack<Character> seventh, Stack<Character> eight, Stack<Character> nineth) {
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(third);
-        System.out.println(forth);
-        System.out.println(fifth);
-        System.out.println(sixth);
-        System.out.println(seventh);
-        System.out.println(eight);
-        System.out.println(nineth);
+        System.out.println("1." + first);
+        System.out.println("2." + second);
+        System.out.println("3." + third);
+        System.out.println("4." + forth);
+        System.out.println("5." + fifth);
+        System.out.println("6." + sixth);
+        System.out.println("7." + seventh);
+        System.out.println("8." + eight);
+        System.out.println("9." + nineth);
     }
 
     private static void generateStacks(Stack<Character> first, Stack<Character> second, Stack<Character> third, Stack<Character> forth, Stack<Character> fifth, Stack<Character> sixth, Stack<Character> seventh, Stack<Character> eight, Stack<Character> nineth) {
