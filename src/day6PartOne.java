7e2501924ffa7ff371da2fe6dfcf2295888d173d
Plamen Tenev <plamen.tenev@accenture.com>
2022-12-07 13:06:18 +0200

    refactoring

--- src/day6PartOne.java
+++ src/day6PartOne.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class day6PartOne {
     public static void main(String[] args) throws IOException {
@@ -16,14 +16,15 @@ public class day6PartOne {
             String[] lineSplitted = line.split("");
             HashSet<String> set = new HashSet<>();
             int answer;
-            for (int i = 0; i < lineSplitted.length - 3; i++) {
+            int sequenceLength = 4;
+            for (int i = 0; i < lineSplitted.length - (sequenceLength - 1); i++) {
                 set.add(lineSplitted[i]);
                 set.add(lineSplitted[i + 1]);
                 set.add(lineSplitted[i + 2]);
                 set.add(lineSplitted[i + 3]);
-                if (set.size() == 4) {
+                if (set.size() == sequenceLength) {
                     System.out.println("Index is " + i);
-                    answer = i + 4;
+                    answer = i + sequenceLength;
                     System.out.println("Answer is: " + answer);
                     break;
                 } else {
@@ -31,10 +32,5 @@ public class day6PartOne {
                 }
             }
         }
-
     }
-
 }
-
-// a b c d e f - length - 6
-//     1563  64 65 66
